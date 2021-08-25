b7120ea2107817af25338c145018abd1bae0ebad
David Njungo <davidnjungo3@gmail.com>
2021-08-25 10:41:33 +0300

    register main blueprint

--- app/__init__.py
+++ app/__init__.py
@@ -5,7 +5,7 @@ from flask_sqlalchemy import SQLAlchemy
 from config import config
 from flask_moment import Moment
 
-#initializing flask extensions
+
 bootstrap = Bootstrap()
 mail = Mail()
 moment = Moment()
@@ -13,12 +13,18 @@ db = SQLAlchemy()
 
 def create_app(config_name):
     app = Flask(__name__)
+
+    # Creating the app configurations
     app.config.from_object(config[config_name])
     config[config_name].init_app(app)
 
+    #initialize flask extensions
     bootstrap.init_app(app)
     mail.init_app(app)
     moment.init_app(app)
     db.init_app(app)
-    # attach routes and custom error pages here
+    
+    #Register the blueprints
+    from .main import main as main_blueprint
+    app.register_blueprint(main_blueprint)
     return app
