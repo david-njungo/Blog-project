acb26ea5c17d7c877d1ab11091c4f0b6efd94f9d
David Njungo <davidnjungo3@gmail.com>
2021-08-25 13:43:18 +0300

    remove file

--- app/__init__.py
+++ app/__init__.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask,render_template
 from flask_bootstrap import Bootstrap
 from flask_mail import Mail
 from flask_sqlalchemy import SQLAlchemy
@@ -27,4 +27,6 @@ def create_app(config_name):
     #Register the blueprints
     from .main import main as main_blueprint
     app.register_blueprint(main_blueprint)
+
+
     return app
