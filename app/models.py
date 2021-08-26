91d7f746ec3bd10905403c1138ec73bc26f253c0
David Njungo <davidnjungo3@gmail.com>
2021-08-27 00:48:29 +0300

    add comment class

--- app/models.py
+++ app/models.py
@@ -38,10 +38,25 @@ class Post(db.Model):
     description = db.Column(db.String(), nullable=False)
     author = db.Column(db.String(), nullable=False)
 
-
-
-
 class Quote:
     def __init__(self,id,quote):
         self.id = id
         self.quote = quote
+
+class Comment(db.Model):
+    __tablename__ = 'comments'
+    id = db.Column(db.Integer, primary_key=True)
+    body = db.Column(db.Text)
+    body_html = db.Column(db.Text)
+    timestamp = db.Column(db.DateTime, index=True, default=datetime.utcnow)
+    disabled = db.Column(db.Boolean)
+    author_id = db.Column(db.Integer, db.ForeignKey('users.id'))
+    post_id = db.Column(db.Integer, db.ForeignKey('posts.id'))
+    @staticmethod
+    def on_changed_body(target, value, oldvalue, initiator):
+        allowed_tags = ['a', 'abbr', 'acronym', 'b', 'code', 'em', 'i',
+        'strong']
+        target.body_html = bleach.linkify(bleach.clean(
+        markdown(value, output_format='html'),
+        tags=allowed_tags, strip=True))
+db.event.listen(Comment.body, 'set', Comment.on_changed_body)
